c794595115879712989f1650eef824abf04a4c46
rodrigogrance <143683364+rodrigogrance@users.noreply.github.com>
2024-01-23 02:40:35 -0300

    Initial commit

---
+++ mod5/src/br/com/rlacorte/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package br.com.rlacorte;
+
+/***
+ * @author rodrigo.grance
+ */
+
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Rodrigo Grance");
+    }
+}
